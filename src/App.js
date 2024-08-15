c06a1e0410c9a7297eab53da8a2d37de1380037b
Anas Khan <akhan2@ischoolconnect.com>
2024-08-15 13:22:40 +0530

    ✨ Navbar implemented with search functionalty

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
     <>
       <Router>
+        <NavBar />
         <Routes>
-          <Route exact path={"/"} element={<div> working </div>} />
+          <Route exact path={"/"} element={<div>work</div>} />
         </Routes>
       </Router>
     </>
